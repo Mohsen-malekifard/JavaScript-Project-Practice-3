44e025cf923d4786440de7077399d2971826337f
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-05 16:24:03 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+for (let i = 1; i<11; i++) {
+    console.log(`With For: ${i}`);
+}
+
+let i = 2;
+while (i <= 10) {
+    console.log(`With While: ${i}`);
+    i += 2;
+}
+//OR
+// let i = 1;
+// while (i <= 10) {
+//     if (i % 2 === 0) {
+//         console.log(`With While: ${i}`);
+//     }
+//     i++;
+// }
